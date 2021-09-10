7cb5ef35315b8b1b89f09d93ea03e5b083e59996
p978 <59633184+p978@users.noreply.github.com>
2021-09-10 12:41:00 +0545

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+import os
+from tkinter import *
+
+root = tk.Tk()
+
+root.configure(background='#1b1b1b')
+root.geometry("600x300")
+
+def Taskmanager():
+    os.system("taskmgr")
+def logout():
+    os.system("logoff")
+def restart():
+    os.system("reboot /s")
+def Shutdown():
+    os.system("shutdown /s")
+def restartexplorer():
+    os.system("taskkill /f /im explorer.exe")
+    os.system("start explorer.exe")
+explorer = tk.Button(root, 
+            width=20,
+            text ="Restart explorer",
+            bg ="#263D42",
+            command=restartexplorer)
+explorer.pack()
+
+Shutdown = tk.Button(root,
+                width=20,
+                text ="Shutdown",
+                bg ="#263D42",
+                command=Shutdown)
+Shutdown.pack()
+
+OpenTaskManager = tk.Button(root,
+                         width=20,
+                         text="Open Task Manager",
+                         bg="#263D42",
+                         command=Taskmanager)
+OpenTaskManager.place(x=1, y=100)
+
+
+
+root.mainloop()
