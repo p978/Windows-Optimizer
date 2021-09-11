273c5d4c2ffa5c525a35e36eb2b2a1d40f361438
p978 <59633184+p978@users.noreply.github.com>
2021-09-11 07:07:29 +0545

    Update Performance.bat

--- Performance.bat
+++ Performance.bat
@@ -1,9 +1,17 @@
-echo "WARNING YOU NEED TO BE ON THE LATEST VERSION TO CONTINUE IF YOU ARE NOT ON THE LATEST VERSION OF WINDOWS THIS MAY BREAK YOUR PC DO YOU WISH TO CONTINIUE DO YOU WISH TO CONTIUE"
-
 @echo off
-:choice
-set /p choice=_<Confirm Message>_
-if /I %choice% EQU "N" exit
-if /I %choice% EQU "Y" (goto continue) else (goto choice)
-goto choice
-
+echo WARINIG THIS MAY BREAK YOUR PC IF YOU ARE NOT AT THE LATEST VERSION OF WINDOWS DO YOU WISH TO CONTINIUE
+wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "<RestorePointName>", 100, 7
+echo A restore point has been created 
+cd Debloat Windows 10
+cd scripts
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+ls -Recurse *.ps*1 | Unblock-File
+./block-telemetry.ps1
+./disable-services.ps1
+./expermimental_unfuckery.ps1
+./fix-privacy-settings.ps1
+./optimize-user-interface.ps1
+./optimize-windows-update.ps1
+./remove-default-apps.ps1
+./remove-onedrive.ps1
+cmd /k
